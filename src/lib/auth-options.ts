7a56bf46d14da5e04204ae4c2e24281d5dc6aa5c
Fidel da Luz <fidel.luz@nosi.cv>
2025-09-16 23:52:30 -0100

    updates

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -18,8 +18,8 @@ export const authOptions: NextAuthOptions = {
 
   session: {
     strategy: 'jwt',
-    maxAge: 4 * 60 * 60, // 4 hours
-    // maxAge: 60,
+    // maxAge: 4 * 60 * 60, // 4 hours
+    maxAge: 60,
   },
 
   cookies: {
