3a517fe40e2367f2eeb9dc678b769f04d7d202e1
Fidel da Luz <fidel.luz@nosi.cv>
2025-09-19 12:24:52 -0100

    update

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -29,7 +29,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: 'lax',
         path: '/',
         //secure: isProd,
-        //...(cookieDomain ? { domain: cookieDomain } : {}),
+        ...(cookieDomain ? { domain: cookieDomain } : {}),
       },
     },
   },
