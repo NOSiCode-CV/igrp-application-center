1f4b8b0427c6cb46fd1878d2798d3a894970b495
Fidel da Luz <fidel.luz@nosi.cv>
2025-09-22 09:30:37 -0100

    updates

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -35,8 +35,10 @@ export const authOptions: NextAuthOptions = {
   },
 
   callbacks: {
-    async redirect({ url, baseUrl }) {      
-      const forced = process.env.NEXTAUTH_URL ?? baseUrl;
+    async redirect({ url, baseUrl }) {   
+      const NEXTAUTH_URL = process.env.NEXTAUTH_URL; 
+      const forced = NEXTAUTH_URL ?? baseUrl;
+      console.log({ NEXTAUTH_URL })
       console.log({ url })
       console.log({ baseUrl })
       console.log({ forced })
